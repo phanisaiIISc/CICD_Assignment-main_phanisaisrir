d039674dfbf0681a7025116e398496857046df24
phanisaiIISc <144316493+phanisaiIISc@users.noreply.github.com>
2024-04-01 15:37:01 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
 
-
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
@@ -14,3 +13,5 @@ model = LogisticRegression().fit(X, y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
+
+exit(1)
